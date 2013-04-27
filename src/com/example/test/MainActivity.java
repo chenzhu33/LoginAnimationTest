d941afc272c0d330172d666b90ed9aaf261617f7
Czw <346402360@qq.com>
2013-04-27 20:51:20 +0800

    Modity

--- src/com/example/test/MainActivity.java
+++ src/com/example/test/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 	private ImageView registerImage;
@@ -23,7 +25,11 @@ public class MainActivity extends Activity {
 	private TranslateAnimation showtime;
 	private int width;
 	private int height;
-
+	private LinearLayout loginInfo;
+	private LinearLayout lbanner;
+	private LinearLayout lbottom;
+	private AlphaAnimation fadeOut;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,61 +39,69 @@ public class MainActivity extends Activity {
 		registerImage = (ImageView) findViewById(R.id.register);
 		loginImage = (ImageView) findViewById(R.id.login);
 		bannerImage = (ImageView) findViewById(R.id.big_c);
+		loginInfo = (LinearLayout)findViewById(R.id.login_info);
+		lbanner = (LinearLayout)findViewById(R.id.banner);
+		lbottom = (LinearLayout)findViewById(R.id.bottom);
 
 		WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 
 		width = wm.getDefaultDisplay().getWidth();
 		height = wm.getDefaultDisplay().getHeight();
 
-		// 加载动画
+		// 加载动画		
 
-		// scrollUp = AnimationUtils.loadAnimation(this,
-		// R.anim.slide_out_to_top);
-		// scrollDown = AnimationUtils.loadAnimation(this,
-		// R.anim.slide_out_to_bottom);
-		scrollUp2 = new TranslateAnimation(0,0,0,-300);
-		scrollUp2.setDuration(1000);
-		scrollUp2.setFillAfter(true);
-		
-		scrollUp = new TranslateAnimation(0,0,0,-300);
-		scrollUp.setDuration(1000);
-		scrollDown = new TranslateAnimation(0,0,0,400);
-		scrollDown.setDuration(1000);
-		scrollUp.setFillAfter(true);
-		scrollDown.setFillAfter(true);
 
-		scrollUp.setAnimationListener(new AnimationListener() {
-			
-			public void onAnimationStart(Animation animation) {
-				
-			}
-			
-			public void onAnimationRepeat(Animation animation) {
-				
-			}
-			
-			public void onAnimationEnd(Animation animation) {
-			    int left = loginImage.getLeft();
-			    int top = loginImage.getTop()-300;
-			    int width = loginImage.getWidth();
-			    int height = loginImage.getHeight();
-			    loginImage.clearAnimation(); //清除动画
-			    loginImage.layout(left, top, left + width, top + height);//重新设置view的大小
-			    loginImage.setClickable(false);
-			}
-		});
+		fadeOut = new AlphaAnimation(0.0f, 1.0f);
+		fadeOut.setStartOffset(400);
+		fadeOut.setDuration(600);
+//		scrollUp.setAnimationListener(new AnimationListener() {
+//			
+//			public void onAnimationStart(Animation animation) {
+//				
+//			}
+//			
+//			public void onAnimationRepeat(Animation animation) {
+//				
+//			}
+//			
+//			public void onAnimationEnd(Animation animation) {
+////			    int left = loginImage.getLeft();
+////			    int top = loginImage.getTop()-300;
+////			    int width = loginImage.getWidth();
+////			    int height = loginImage.getHeight();
+////			    loginImage.clearAnimation(); //清除动画
+////			    
+////			    loginImage.layout(left, top, left + width, top + height);//重新设置view的大小
+//			    //loginImage.setClickable(false);
+//			}
+//		});
 		
 		loginImage.setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				int[] location = new int[2];
-				loginImage.getLocationOnScreen(location);// 获取在整个屏幕内的绝对坐标
-				loginImage.startAnimation(scrollUp);
+			    int infoTop = loginInfo.getTop();
+			    int bannerBottom = lbanner.getBottom();			    
+			    int distance = bannerBottom - infoTop;
+			    
+			    int infoBottom = loginInfo.getBottom();
+			    int lbottomTop = lbottom.getTop();			    
+			    int distance2 = infoBottom - lbottomTop;
+			    
+			    
+				scrollUp = new TranslateAnimation(0,0,0,-distance);
+				scrollUp.setDuration(600);
+				scrollUp.setFillAfter(true);
 				
-				registerImage.startAnimation(scrollDown);
-				bannerImage.startAnimation(scrollUp2);
+				scrollDown = new TranslateAnimation(0,0,0,distance2);
+				scrollDown.setDuration(600);
+				//scrollUp.setFillAfter(true);
+				scrollDown.setFillAfter(true);
+			    
+				lbanner.startAnimation(scrollUp);
+				lbottom.startAnimation(scrollDown);
 			}
 		});
 
 	}
+
 }
